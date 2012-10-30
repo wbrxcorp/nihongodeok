58049fc85620bb84da6ce53483c3fe14b23f2773
Tomoatsu Shimada <shimada@walbrix.com>
2012-10-30 18:10:30 +0530

    cnn example

---
+++ scraper/cnn.py
@@ -0,0 +1,11 @@
+def scrape(soup):
+    subject = soup.findAll('h1')[0].string
+    paragraphs = []
+    first_one = soup.findAll('p', attrs={'class': None})
+    if len(first_one) > 0: paragraphs.append(first_one[0])
+    paragraphs += soup.findAll('p', attrs={'class': re.compile(r'\bcnn_storypgraphtxt\b')})
+    if len(paragraphs) == 0: return None
+
+    body = "\n\n".join(map(lambda x:''.join(x.findAll(text=True)), paragraphs))
+
+    return (subject, body, None)
